3b513e3922272405564befea8fe6b93153764b63
Jesse Griffin <jag3773@gmail.com>
2016-12-15 10:57:39 -0700

    enabled newly added modules

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,11 @@
 # Base class for all servers
 node default {
     include sysctl
-
-    #include users git 2024
-    #include nginx simple 80 ->443
-    #include runpuppet
-    #include postfix
-    #include opsview
-    #include usrlocalsbin
-    #include miscpkgs
-    #include repos::nagios_plugins
+    include users
+    include nginx
+    include puppet
+    include opsview
     #include s3backup
-    #include torrent
-    #include tor
     #include btsync # if included needs user and root directory. see sp.door43.org
     #include gogs
 }
