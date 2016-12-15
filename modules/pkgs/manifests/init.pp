74c5a270455c6d8064e7dfbd531b202c0a44bf96
Jesse Griffin <jag3773@gmail.com>
2016-12-15 10:44:55 -0700

    Added default list of packages

---
+++ modules/pkgs/manifests/init.pp
@@ -0,0 +1,26 @@
+class pkgs {
+
+    package { git: ensure  => present }
+    package { puppet: ensure  => present }
+    package { rsync: ensure  => present }
+    package { screen: ensure  => present }
+    package { telnet: ensure  => present }
+    package { wget: ensure  => present }
+    package { mysql: ensure  => present }
+    package { nfs-utils: ensure  => present }
+    package { nginx: ensure  => present }
+    #package { s3cmd: ensure  => present }
+    package { iftop: ensure  => present }
+    package { tmux: ensure => present }
+    package { 'vim-enhanced': ensure  => present }
+    package { 'ruby-shadow': ensure => present }
+
+    file { '/etc/tmux.conf':
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source  => "puppet:///modules/miscpkgs/tmux.conf",
+        require => Package['tmux'],
+    }
+
+}
