c1e5c62a0011c974bbbd4ef88a93a54edba1707e
Jesse Griffin <jag3773@gmail.com>
2016-12-15 11:00:09 -0700

    added pkgs module to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,7 @@ node default {
     include nginx
     include puppet
     include opsview
+    include pkgs
     #include s3backup
     #include btsync # if included needs user and root directory. see sp.door43.org
     #include gogs
