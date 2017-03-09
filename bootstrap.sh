fa6ea04b815b2fd2b2062afb01170dcf360e6794
Jesse Griffin <jag3773@gmail.com>
2017-03-09 13:02:49 -0700

    Added small swap space to nodes

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,13 @@ yum -y install rsync screen tmux vim-enhanced telnet wget mysql puppet git nfs-u
 echo 'mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-acfe3f05.efs.us-west-2.amazonaws.com:/ /mnt/' >> /etc/rc.local
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-acfe3f05.efs.us-west-2.amazonaws.com:/ /mnt/
 
+# Setup small swap space
+dd if=/dev/zero of=/swapfile bs=1M count=512
+chmod 600 /swapfile
+mkswap /swapfile
+echo '/swapfile   none        swap    sw              0   0' >> /etc/fstab
+swapon -a
+
 # Link puppet
 chkconfig puppet off
 rm -rf /etc/puppet
