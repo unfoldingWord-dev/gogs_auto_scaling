649d8419b0d976b07bae909f188eb95ea09e1ffc
Jesse Griffin <jag3773@gmail.com>
2017-03-16 16:04:36 -0600

    switching to single t2.med instance and ebs volume

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@
 # Install necessary system packages
 yum -y install rsync screen tmux vim-enhanced telnet wget mysql puppet git nfs-utils nginx
 
-# Mount EFS share to /mnt
-echo 'mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-acfe3f05.efs.us-west-2.amazonaws.com:/ /mnt/' >> /etc/rc.local
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-acfe3f05.efs.us-west-2.amazonaws.com:/ /mnt/
+# Mount EBS volume
+echo 'UUID=feb25d8e-68b2-424c-a2cb-37619cd6741a /mnt ext4 defaults,noatime,nofail 0 2' >>/etc/fstab
+mount -a
 
 # Setup small swap space
 dd if=/dev/zero of=/swapfile bs=1M count=512
@@ -13,6 +13,9 @@ chmod 600 /swapfile
 mkswap /swapfile
 echo '/swapfile   none        swap    sw              0   0' >> /etc/fstab
 swapon -a
+
+# Verify EBS volume mounted
+[ -d /mnt/ssh ] || exit 1
 
 # Get latest puppet
 chkconfig puppet off
