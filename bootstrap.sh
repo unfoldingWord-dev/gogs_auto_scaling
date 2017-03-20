a5bd27ab34f19604d2d42238097affb69399b074
Jesse Griffin <jag3773@gmail.com>
2017-03-20 16:56:01 -0600

    Refactored storage so that config remains on EFS
    This separates the Gogs data which remains on an EBS volume from
    the general system configuration which is on a shared EFS volume.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,6 @@
 # Install necessary system packages
 yum -y install rsync screen tmux vim-enhanced telnet wget mysql puppet git nfs-utils nginx
 
-# Mount EBS volume
-echo 'UUID=feb25d8e-68b2-424c-a2cb-37619cd6741a /mnt ext4 defaults,noatime,nofail 0 2' >>/etc/fstab
-mount -a
-
 # Setup small swap space
 dd if=/dev/zero of=/swapfile bs=1M count=512
 chmod 600 /swapfile
@@ -14,28 +10,28 @@ mkswap /swapfile
 echo '/swapfile   none        swap    sw              0   0' >> /etc/fstab
 swapon -a
 
-# Verify EBS volume mounted
-[ -d /mnt/ssh ] || exit 1
-
 # Get latest puppet
 chkconfig puppet off
 rm -rf /etc/puppet
 git clone https://github.com/unfoldingWord-dev/gogs_auto_scaling.git /etc/puppet
 
+# Mount configuration volume
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-acfe3f05.efs.us-west-2.amazonaws.com:/ /config/
+
 # Link SSH
-rsync -havP --delete /mnt/ssh/ /etc/ssh/
+rsync -havP --delete /config/ssh/ /etc/ssh/
 service sshd restart
-/bin/cp -f /mnt/private/authorized_keys /root/.ssh/
-/bin/cp -f /mnt/private/authorized_keys /home/ec2-user/.ssh/
+/bin/cp -f /config/private/authorized_keys /root/.ssh/
+/bin/cp -f /config/private/authorized_keys /home/ec2-user/.ssh/
 chown ec2-user:ec2-user /home/ec2-user/.ssh/authorized_keys
-/bin/cp -f /mnt/private/my.cnf /etc/
+/bin/cp -f /config/private/my.cnf /etc/
 
 # Run puppet
 puppet apply /etc/puppet/manifests/site.pp --summarize
 puppet apply /etc/puppet/manifests/site.pp -e 'include gogs' --summarize
 
-# Set Gogs to start on boot
-echo '/etc/init.d/gogs restart' >> /etc/rc.local
+# Add mount for EBS volume
+echo 'UUID=feb25d8e-68b2-424c-a2cb-37619cd6741a /mnt ext4 defaults,noatime,nofail 0 2' >>/etc/fstab
 
 # Reboot for good measure
 reboot
