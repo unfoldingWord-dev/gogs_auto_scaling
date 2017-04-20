666292f40d58d7ca24825a21113506f72d058f3f
Jesse Griffin <jag3773@gmail.com>
2017-04-20 16:23:35 -0600

    Removed some pkgs that are in bootstrap

--- modules/pkgs/manifests/init.pp
+++ modules/pkgs/manifests/init.pp
@@ -6,14 +6,8 @@ class pkgs {
     package { screen: ensure  => present }
     package { telnet: ensure  => present }
     package { wget: ensure  => present }
-    package { mysql: ensure  => present }
-    package { nfs-utils: ensure  => present }
     package { nginx: ensure  => present }
-    #package { s3cmd: ensure  => present }
-    #package { iftop: ensure  => present }
     package { tmux: ensure => present }
-    package { 'vim-enhanced': ensure  => present }
-    #package { 'ruby-shadow': ensure => present }
 
     file { '/etc/tmux.conf':
         owner   => root,
