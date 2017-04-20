70cda1313ef50d11a08a13e633029266a404a412
Jesse Griffin <jag3773@gmail.com>
2017-04-20 16:38:44 -0600

    More changes to abstract modules

--- modules/sysctl/manifests/init.pp
+++ modules/sysctl/manifests/init.pp
@@ -1,15 +1,15 @@
 class sysctl {
 
-    file { '/etc/sysctl.conf':
+    file { '/etc/sysctl.d/60-dcs.conf':
         owner   => root,
         group   => root,
         mode    => 444,
-        source  => "puppet:///modules/sysctl/sysctl.conf",
+        source  => "puppet:///modules/sysctl/60-dcs.conf",
         notify  => Exec['sysctl'],
     }
 
     exec { 'sysctl':
-        command     => '/sbin/sysctl -p',
+        command     => '/sbin/sysctl -p /etc/sysctl.d/60-dcs.conf',
         refreshonly => true,
         user        => root,
         group       => root,
